7f4fc725f24e86e6e64d024173866979141af63d
Aleksander Alvarez <alekalvarez123@gmail.com>
2024-04-20 15:35:14 -0500

    Links html css and script files together for the purpose of testing math functions

---
+++ script.js
@@ -0,0 +1,16 @@
+function add(Addend1,Addend2){
+    let sum=Addend1+Addend2;
+    return sum;
+}
+function subtract(minuend,subtrahend){
+    let difference=minuend-subtrahend;
+    return difference;
+}
+function multiply(multiplicand, multipier){
+    let product=multiplicand*multipier;
+    return product;
+}
+function divide(dividend,divisor){
+    let quotient=dividend/divisor;
+    return quotient;
+}
